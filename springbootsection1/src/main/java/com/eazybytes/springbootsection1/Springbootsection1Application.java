e511c0d25f9d0c1a0158344ffbbb07c3924dc3b0
hardikyadav12 <hardik.ind12@gmail.com>
2024-12-29 08:44:06 +0530

    first commit

---
+++ springbootsection1/src/main/java/com/eazybytes/springbootsection1/Springbootsection1Application.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springbootsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootsection1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootsection1Application.class, args);
+	}
+
+}
---
+++ springbootsection1/src/test/java/com/eazybytes/springbootsection1/Springbootsection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springbootsection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootsection1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
