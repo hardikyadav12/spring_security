c402d731973c034277c317b67159d1a90fdc6372
hardikyadav12 <hardik.ind12@gmail.com>
2024-12-31 10:03:26 +0530

    filter to auths & increasing log level

--- src/main/java/com/eazybytes/springbootsection1/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/springbootsection1/config/ProjectSecurityConfig.java
@@ -3,6 +3,7 @@ package com.eazybytes.springbootsection1.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,8 +13,9 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/contact/**","/notices/**").permitAll());
-        http.formLogin(withDefaults());
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/contact/**","/notices/**","/error").permitAll()
+                .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated());
+        http.formLogin(AbstractHttpConfigurer::disable);
         http.httpBasic(withDefaults());
         return http.build();
     }
